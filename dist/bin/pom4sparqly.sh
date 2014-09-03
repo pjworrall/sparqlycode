6737da90023f1ce81a143f990dbbc1e34d087d0a
Paul Worrall <paul.worrall@interition.net>
2014-09-03 13:30:53 +0100

    made JAVA_VERSION default to 1.7

--- dist/bin/pom4sparqly.sh
+++ dist/bin/pom4sparqly.sh
@@ -21,7 +21,7 @@ if [ -z "$SPARQLYCODE_HOME" ] ; then
 fi
 
 #USAGE="Usage: `basename $0` package BaseUri"
-USAGE="Usage: `basename $0` package"
+USAGE="Usage: `basename $0` package [java_version]"
 
 if [ ! "$1" ] ; then
    echo You need to provide a java root package  . Eg net.interition.sparqlycode
@@ -29,20 +29,22 @@ if [ ! "$1" ] ; then
    exit 1;
 fi
 
-if [ ! "$2" ] ; then
-   echo You need to provide a Java version
-   echo $USAGE >&2
-   exit 1;
+JAVA_VERSION=$2
+
+if [ "$JAVA_VERSION" = "" ] ; then
+   echo "No Java version provided, defaulting to 1.7"
+   JAVA_VERSION=1.7
 fi
 
+# We are not providing baseuri
 #if [ ! "$3" ] ; then
 #   echo You need to provide a BaseUri. Eg. the source code repository uri
 #   echo $USAGE >&2
 #   exit 1;
 #fi
 
-#export params="SPARQLYCODE_HOME=${SPARQLYCODE_HOME} SUBPACKAGE=$1 JAVADOC_VERSION=$2 BASEURI=$3"
-export params="SPARQLYCODE_HOME=${SPARQLYCODE_HOME} SUBPACKAGE=$1 JAVADOC_VERSION=$2"
+#export params="SPARQLYCODE_HOME=${SPARQLYCODE_HOME} SUBPACKAGE=$1 JAVADOC_VERSION=$JAVA_VERSION BASEURI=$3"
+export params="SPARQLYCODE_HOME=${SPARQLYCODE_HOME} SUBPACKAGE=$1 JAVADOC_VERSION=$JAVA_VERSION"
 echo "\n"
 
 $JAVA_HOME/bin/java -jar $SAXON_HOME/saxon9he.jar -xsl:${SPARQLYCODE_HOME}/bin/pom4sparqly.xsl -s:pom.xml -o:scpom.xml $params 
